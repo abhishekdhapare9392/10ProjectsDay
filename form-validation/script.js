324b2468b76f1bfc4d487b0f72fc3efeb9075e80
abhishekdhapare9392 <abhishek.dhapare@gmail.com>
2020-09-18 16:30:14 +0530

    form validation

--- form-validation/script.js
+++ form-validation/script.js
@@ -1,34 +1,41 @@
 // Import all element from DOM
-const firstName = document.getElementById('firstname');
-const lastName = document.getElementById('lastname');
-const email = document.getElementById('email');
-const message = document.getElementById('message');
+var firstName = document.querySelector('#firstname');
+var lastName = document.querySelector('#lastname');
+var email = document.querySelector('#email');
+var message = document.querySelector('#message');
 const errorMsg = document.querySelector('.error-msg');
-const submitBtn = document.getElementById('submit');
+const submitBtn = document.querySelector('#submit');
+
+const regex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 // On click event for submit button
 submitBtn.addEventListener('click', (e) => {
-  console.log('testing');
-  if (
-    firstName.value === '' ||
-    lastName.value === '' ||
-    email.value === '' ||
-    message.value === ''
-  ) {
-    console.log('testing');
+  firstName = document.querySelector('#firstname').value;
+  lastName = document.querySelector('#lastname').value;
+  email = document.querySelector('#email').value;
+  message = document.querySelector('#message').value;
+
+  if (regex.test(email) === false) {
+    showAlert(
+      'Email format is wrong. All fields required.',
+      'alert alert-danger'
+    );
+  }
+  if (firstName === '' || lastName === '' || email === '' || message === '') {
     showAlert(
       'First name is not filled. All fields required',
       'alert alert-danger'
     );
-    // set time out for alerts
-    setTimeout(() => {
-      let alerts = document.querySelectorAll('.alert');
-
-      alerts.forEach((alert) => {
-        alert.remove();
-      });
-    }, 5000);
   }
+
+  // set time out for alerts
+  setTimeout(() => {
+    let alerts = document.querySelectorAll('.alert');
+
+    alerts.forEach((alert) => {
+      alert.remove();
+    });
+  }, 5000);
 
   e.preventDefault();
 });
@@ -39,5 +46,5 @@ const showAlert = (msg, className) => {
   let div = document.createElement('div');
   div.className = className;
   div.innerText = msg;
-  errorMsg.append(div);
+  errorMsg.appendChild(div);
 };
